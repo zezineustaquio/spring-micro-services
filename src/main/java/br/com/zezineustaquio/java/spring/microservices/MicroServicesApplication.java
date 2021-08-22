8ab18dbe7848758517fd0af1acb8d65d67b72c9f
Jose Eustaquio <jose.medeiros@sevenmmt.com>
2021-08-22 13:50:07 -0300

    Projeto inicial criado em https://start.spring.io

---
+++ src/main/java/br/com/zezineustaquio/java/spring/microservices/MicroServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zezineustaquio.java.spring.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zezineustaquio/java/spring/microservices/MicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zezineustaquio.java.spring.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
